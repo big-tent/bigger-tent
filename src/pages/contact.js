f6c8e32600616cb39db64a8d7dfc525a9296bcb7
djc74 <davidcooper974@gmail.com>
2018-10-15 23:04:06 +0100

    contact form started

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,10 +2,31 @@ import React from "react"
 
 import Layout from "../components/Layout"
 import Header from "../components/Header"
+import Topper from "../components/Topper"
 
 export default () => (
   <Layout>
+    <Topper color="black" />
     <Header headerText="Contact us" />
-    <p>Say hello</p>
+    <form name="contact" method="POST" netlify>
+      <p>
+        <label>
+          Your Name: <input type="text" name="name" placeholder="name" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Your Email: <input type="email" name="email" placeholder="email" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Message: <textarea name="message" />
+        </label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
   </Layout>
 )
