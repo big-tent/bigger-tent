0e75aa8669ad9f52edfa55e364b4a76f1adc91b8
djc74 <davidcooper974@gmail.com>
2019-03-09 08:23:07 +0000

    reducing opacity on footer copy

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,34 +8,18 @@ const Foot = styled.footer`
   left: 0;
   position: relative;
   width: 100%;
-  /* :before {
-    background: inherit;
-    bottom: 0;
-    content: "";
-    display: block;
-    height: 100%;
-    left: 0;
-    position: absolute;
-    right: 0;
-    transform: skewY(2deg);
-    transform-origin: 100%;
-  } */
   clip-path: polygon(0 45%, 100% 0, 100% 100%, 0% 100%);
 `
 
 const FooterCopy = styled.h6`
   bottom: 0;
-  color: #fefefe;
+  color: hsla(0, 0%, 99%, 0.7);
   font-weight: 100;
   position: absolute;
   right: 2vw;
 `
 
-const HeartsCopy = styled.h6`
-  bottom: 0;
-  color: #fefefe;
-  font-weight: 100;
-  position: absolute;
+const HeartsCopy = styled(FooterCopy)`
   left: 2vw;
 `
 
