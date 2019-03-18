0924cbe1bbad8c6ff7a323bcaf1b0b3aee6f4903
djc74 <davidcooper974@gmail.com>
2019-03-18 21:05:08 +0000

    adding grey circle logo to manifest

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -99,6 +99,14 @@ const BlogPost = ({ node }) => {
   )
 }
 
+const CarouselImage = ({ node }) => {
+  return (
+    <li>
+      <Img fluid={node.image.fluid} />
+    </li>
+  )
+}
+
 export default ({ data, location }) => (
   <Layout location={location}>
     <BlogPageWrapper>
@@ -111,6 +119,12 @@ export default ({ data, location }) => (
             <BlogPost node={edge.node} />
           ))}
         </BlogPostList>
+
+        <ul>
+          {data.allContentfulBlog.edges.map(edge => (
+            <CarouselImage node={edge.node} />
+          ))}
+        </ul>
       </ContentHolder>
       <Footer color="hsla(0,0%,8.6%,0.7)" />
     </BlogPageWrapper>
