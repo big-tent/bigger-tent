b61d05abf94fafd83bc34cec841aa3f82948568f
djc74 <davidcooper974@gmail.com>
2019-04-26 11:52:19 +0100

    added audio page

---
+++ src/pages/audio.js
@@ -0,0 +1,104 @@
+import React from "react"
+import { graphql } from "gatsby"
+import SEO from "../components/seo"
+
+import Layout from "../components/layout"
+import Topper from "../components/topper"
+import Nav from "../components/nav"
+import Header from "../components/header"
+import ContentHolder from "../components/contentholder"
+import BlurbMain from "../components/blurbmain"
+import Footer from "../components/footer"
+import BannerPic from "../components/bannerimage"
+import VideoHolder from "../components/videosholder"
+import VideoSection from "../components/vidsection"
+
+export default ({ location, data }) => (
+  <Layout location={location}>
+    <SEO
+      title="Audio"
+      keywords={[`voice `, `audio`, `flash briefing`, `Alexa`]}
+    />
+    <Topper color="hsla(0, 0%, 8.6%, 0.8)" />
+    <Nav navcolor="hsl(31.6, 92.5%, 52.5%)" />
+    <BannerPic
+      fluid={data.bannerImage.childImageSharp.fluid}
+      alt="Child drawing"
+    />
+    <ContentHolder>
+      <BlurbMain>
+        <Header headerText="Audio" />
+        <div
+          dangerouslySetInnerHTML={{
+            __html: data.headerText.childMarkdownRemark.html
+          }}
+        />
+      </BlurbMain>
+    </ContentHolder>
+    <VideoSection color="hsl(30, 100%, 95%)" heading="Animations">
+      <VideoHolder publicId="Videos/1f_getting-bedtime-off-to-a-flying-start" />
+      <VideoHolder publicId="Videos/alluxi_1" />
+    </VideoSection>
+    <VideoSection
+      color="hsl(30, 100%, 95%)"
+      heading="Audiograms"
+      copy={
+        <p
+          dangerouslySetInnerHTML={{
+            __html: data.animationText.childMarkdownRemark.html
+          }}
+        />
+      }
+    >
+      <VideoHolder publicId="Videos/A_bit_about_the_product" />
+      <VideoHolder publicId="Videos/Copy_of_Soti_post_TEDx" />
+      <VideoHolder publicId="Videos/March_1_Social_Days_Made_by_Headliner" />
+    </VideoSection>
+    <VideoSection
+      color="hsl(30, 100%, 95%)"
+      heading="Videos"
+      copy={
+        <p
+          dangerouslySetInnerHTML={{
+            __html: data.animationText.childMarkdownRemark.html
+          }}
+        />
+      }
+    >
+      <VideoHolder publicId="Videos/wk1_Water" />
+      <VideoHolder publicId="Videos/wk2_summer-sports-mason-clinic_1" />
+    </VideoSection>
+    <Footer color="hsl(30, 100%, 50%)" />
+  </Layout>
+)
+
+export const query = graphql`
+  fragment AudioCopy on File {
+    childMarkdownRemark {
+      frontmatter {
+        title
+      }
+      html
+    }
+  }
+
+  query AudioQuery {
+    headerText: file(
+      relativePath: { eq: "markdown/content/contentheader.md" }
+    ) {
+      ...ContentCopy
+    }
+    animationText: file(
+      relativePath: { eq: "markdown/content/contentanimation.md" }
+    ) {
+      ...ContentCopy
+    }
+    bannerImage: file(relativePath: { eq: "rawpixel-463437-unsplash.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 1000) {
+          ...GatsbyImageSharpFluid_withWebp_tracedSVG
+        }
+      }
+    }
+  }
+`
